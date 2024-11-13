cdf677ae197b0ec9f52c3d815097bd88dca10872
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-13 17:30:55 -0600

    first couple of methods

--- main.cpp
+++ main.cpp
@@ -6,26 +6,59 @@ using namespace std;
 
 class Main {
 private:
-  expression=
 
 public:
-  // 1. Read values from stdin into a matrix
-  string getInput();
+	//makes first call to input processor class
+	string callInput();
 
-  // 2. Clean input str
-  int cleanInput(string);
-
-  // 3. Check input str
-  int checkInput(string);
-
-	//getter (defined in class)
-
-	//setter (defined in class)
+	//decisions on what input processor returns bellow: 
 	
 };
 
-string InputProcessor::getInput(){
+string Main::callInput() //edit this to call class InputProcessor 
+{
 	// some of this code is reusable so
+	string mat_str;
+	getline(cin, mat_str);
+	istringstream iss(mat_str);
+	int x;
+	int row_num = 0;
+	int col_num = 0;
+	while (iss >> x) {
+		if (row_num >= SIZE) {
+			// if input size is too large, return
+			return;
+		}
+		setMatrixElement(row_num, col_num, x);
+		col_num++;
+		if (col_num == SIZE) {
+			//iterates 1 through (SIZE-1) for both column and row
+			col_num = 0;
+			row_num++;
+		}
+	}
+}
+
+void main()
+{
+	Main start; //starts the program 
+	start.callInput();
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+void Main::getInput(){
+	// some of this code is reusable so 
+	
 	string mat_str;
 	getline(cin, mat_str);
 	istringstream iss(mat_str);
@@ -54,3 +87,5 @@ int InputProcessor::cleanInput(string) {
 int InputProcessor::checkInput(string){
 	
 }
+
+
