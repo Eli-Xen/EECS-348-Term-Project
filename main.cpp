85cc68ee8bde6b6702c6d0f27c6bf8b05f09b387
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-14 11:05:05 -0600

    added call to InputProcessor

--- main.cpp
+++ main.cpp
@@ -16,26 +16,6 @@ public:
 	//makes first call to input processor class
 	string callInput();
 
-	//decisions on what input processor returns bellow: 
-
-
-	//Method for getting the user input from terminal
-	string getInput();
-	
-	//Setters for string varables
-	void setCleanInput(string data){
-		cleanInput = data;
-	}
-	void setUncleanInput(string data){
-		uncleanInput = data;
-	}
-	//Getters for string varables
-	string getCleanInput(){
-		return cleanInput;
-	}
-	string getUncleanInput(){
-		return uncleanInput;
-	}
 };
 
 
@@ -44,32 +24,15 @@ void main()
 {
 	Main start; //starts the program 
 	start.callInput();
-	
+	return 0; 
 }
 
 
 string Main::callInput() //edit this to call class InputProcessor 
 {
-	// some of this code is reusable so
-	string mat_str;
-	getline(cin, mat_str);
-	istringstream iss(mat_str);
-	int x;
-	int row_num = 0;
-	int col_num = 0;
-	while (iss >> x) {
-		if (row_num >= SIZE) {
-			// if input size is too large, return
-			return;
-		}
-		setMatrixElement(row_num, col_num, x);
-		col_num++;
-		if (col_num == SIZE) {
-			//iterates 1 through (SIZE-1) for both column and row
-			col_num = 0;
-			row_num++;
-		}
-	}
+	InputProcessor inputProcessor
+	string expression=inputProcessor.processInput() //change the name of method based on what its called 
+	return expression; 
 }
 
 string Main::getInput(){
