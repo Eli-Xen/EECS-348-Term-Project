496a385aac00101d456cd3ff4a504e67a9c48243
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-14 20:46:36 -0600

    defined header with current methods

--- inputProcessor.h
+++ inputProcessor.h
@@ -1,3 +1,18 @@
 #ifndef INPUTPROCESSOR_H 
 #define INPUTPROCESSOR_H 
 
+#include <string> //so we can return strings from functions 
+using namespace std;
+
+class InputProcessor
+{
+private:
+  extern string uncleanInput;
+public: 
+	string getInput();
+	int cleanInput(string);
+	int checkInput(string);
+	void setUncleanInput(string data); //setter 
+	string getUncleanInput(); //getter
+}; 
+#endif
