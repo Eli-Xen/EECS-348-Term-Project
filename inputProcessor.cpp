9e22768f8ca40fb9d6258c713624365636047ea2
Lillian Brooks-Kanost <142836967+lbrookskanost@users.noreply.github.com>
2024-11-15 20:29:34 -0600

    Update inputProcessor.cpp

--- inputProcessor.cpp
+++ inputProcessor.cpp
@@ -13,13 +13,16 @@ private:
 
 public:
 	// 1. Read values from stdin into a private variable uncleanInput
-	string getInput();
+	void getInput();
 
 	// 2. Clean input str
 	void cleanInput();
 
 	// 3. Check input str
 	int checkInput();
+
+	//could also make a run function
+	int run();
 
 	//setter (defined in class)
 	void setUncleanInput(string data){
@@ -32,14 +35,20 @@ public:
 	
 };
 
-string InputProcessor::getInput(){
+int InputProcessor::run(){
+	getInput();
+	cleanInput();
+	int check = checkInput();
+
+}
+
+void InputProcessor::getInput(){
 	//Ask user for input
 	cout << "Enter expression: ";
 	string tempStorage;
 	getline(cin, tempStorage);
 	//Use setter to set private varable
 	setUncleanInput(tempStorage);
-	cleanInput();
 }
 
 void InputProcessor::cleanInput() {
@@ -61,19 +70,31 @@ int InputProcessor::checkInput(){
 	string tempStorage = getUncleanInput();
 	if (tempStorage == "q") || (tempStorage == "Q") //to quit program
 		return -1; //sends to getInput function and returns to main to indicate quit 
-	for (int i = 0; i < (tempStorage.length() - 1); i++) {
+	for (int i = 0; i < tempStorage.length(); i++) {
 		//trying to convert to ascii this might not work if str slicing is not type char
 		if (36 < int(tempStorage[i]) < 58) {
 			//means its either a number or mostly valid numerical chars
 			if (tempStorage[i] != "'" && tempStorage[i] != "," && tempStorage[i] != "-") {
 				//chars are all valid
-				//this is terrible aww no this is clever :( 
-				//return 1; //indicates that input is valid 
-				//other checks needed ie operator check like += and random stuff 
+				//now testing if two invalid operators r placed together
+				//doesn't check if the final char is adjacent 
+				if (i == tempStorage.length()-1)
+					continue;
+				//if the slice is a non-number and the next slice is a non-number- again might not work if string slicing doesn't cast to char
+				if (int(tempStorage[i] < 48 ) && (int(tempStorage[i+1] < 48) {
+					//if any of these two are placed side by side, its valid, so it continues
+					if (tempStorage[i] == "*" && tempStorage[i+1] == "*") || (tempStorage[i] == ")" && tempStorage[i+1] == ")") || (tempStorage[i] == "(" && tempStorage[i+1] == "(")
+						continue;
+				}
+				//numbers automatically continue
+				continue;
 				
 			}
 		}
+		//if it didn't hit any of the continues and successfully exits the loop, something was messed up, so it returns error
+		return 1;
 	}
+	//all chars validated successfully
 	return 0;
 }
 
