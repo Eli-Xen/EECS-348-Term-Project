dacbf6a1b4ec1d4ddc63011f1fc755f771838cc5
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-19 16:59:46 -0600

    minor error fixing

--- expo.cpp
+++ expo.cpp
@@ -1,5 +1,4 @@
 #include "expo.h" //header file for this file
-#include<bits/stdc++.h> //https://www.geeksforgeeks.org/cpp-string-to-float-double-and-vice-versa/#
 #include <string> //so we can return strings from functions 
 #include <iostream>
 #include <sstream> 
@@ -7,29 +6,30 @@ using namespace std;
 
 class Expo {
 private: 
-    string expression;
+    string expression; //considering deleting this since we dont use it 
 
 public:
-
-    template <typename T> T evlExponent(T base, T power);
-
+    string evlExponent(string base, string power)
+    //template <typename T> T evlExponent(T base, T power);
+    //https://www.geeksforgeeks.org/cpp-string-to-float-double-and-vice-versa/# 
 };
 
-template <typename T> T Expo::evlExponent(T base, T power){
+
+string Expo::evlExponent(string base, string power) {
 //takes in operand and left (base) and right (exponent) 
     //loop that multiplies base by iteself exponent amount of times 
     //acommodate for negative exponents: solve as normal loop and then divide 1 by that result then return that 
     double base=stod(base);
     int power=stoi(power); //assumes power is pos/neg int 
     int neg=0; //tracks of power is negative, initially set to 0
+    double result=1.0; 
     if (power<0)
-        {power*-1; neg+=1;} //if power is negative set neg to True/1 
-    for(int i; i<power; i++)
-        {base*base;}
+        {power=power*-1; neg+=1;} //if power is negative set neg to True/1 
+    for(int i=0; i<power; i++)
+        {result*=base;}
     if (neg>0) //happens if power<0 
-        {base=(1/base);} //divides 1 by the result to account for netaive
-    string result=to_string(base); 
-    return result; 
+        {result=(1.0/base);} //divides 1 by the result to account for netaive
+    return to_string(result); 
     
 }
 
