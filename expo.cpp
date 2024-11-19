5d720aab3dfb8cc17a42437aa196e02697f05967
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-19 13:36:16 -0600

    untested draft of exponent
    converts string to double/int 
    checks if power is negative (assumed to be int) 
    for loop that multiplies base by itself power amount of times 
    if power was negative then divide 1 by the result 
    convert result to string and return that

--- expo.cpp
+++ expo.cpp
@@ -1,24 +1,32 @@
 #include "expo.h" //header file for this file
+#include<bits/stdc++.h> 
 using namespace std;
 
 class Expo {
-private:
-    //I'm not sure what type of data we want to pass into these classes
-    //For now I left it as string. Feel free to change as needed
+private: 
     string expression;
 
 public:
 
     template <typename T> T evlExponent(T base, T power);
 
-
-
-
 };
 
 template <typename T> T Expo::evlExponent(T base, T power){
 //takes in operand and left (base) and right (exponent) 
     //loop that multiplies base by iteself exponent amount of times 
-    //acommodate for negative exponents 
+    //acommodate for negative exponents: solve as normal loop and then divide 1 by that result then return that 
+    double base=stod(base);
+    int power=stoi(power); //assumes power is pos/neg int 
+    int neg=0; //tracks of power is negative, initially set to 0
+    if (power<0)
+        {power*-1; neg+=1;} //if power is negative set neg to True/1 
+    for(int i; i<power; i++)
+        {base*base;}
+    if (neg>0) //happens if power<0 
+        {base=(1/base);} //divides 1 by the result to account for netaive
+    string result=to_string(base); 
+    return result; 
+    
 }
 
