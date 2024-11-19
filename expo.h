d56cd1051b6f089e5c95e8321d466936d44cbce8
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-19 16:10:58 -0600

    Update expo.h

--- expo.h
+++ expo.h
@@ -2,7 +2,10 @@
 #ifndef EXPO_H //checks if EXPO_H has alrdy been defined, if true then this file will be skipped 
 #define EXPO_H //starts defenition for header file 
 
+#include<bits/stdc++.h> 
 #include <string> //so we can return strings from functions 
+#include <iostream>
+#include <sstream> 
 using namespace std;
 
 class Expo {
