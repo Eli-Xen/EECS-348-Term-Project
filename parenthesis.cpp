9fe78d7054612ea7e4f578e6a2f84b360bfcb435
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-19 15:57:31 -0600

    Create parenthesis.cpp

---
+++ parenthesis.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Parenthesis {
+private:
+	//A varable to store the clean input after InputProcessor is called
+	string fullExpression;
+  string subExpression; 
+
+public:
+	string callInput(); 
+	string callParenthesis(); 
+
+	//Setters for string varables
+	void setCleanInput(string data){
+		cleanInput = data;
+	}
+
+	//Getters for string varables
+	string getCleanInput(){
+		return cleanInput;
+	}
+
+};
+
+
+string Main::callInput() //edit this to call class InputProcessor 
+{
+	InputProcessor inputProcessor
+	string expression=inputProcessor.run() //change the name of method based on what its called 
+	//inputProcessor will return an empty string to quit the program, the string "input error" if input is invalid,
+	//or the cleaned/validated string if the input is good
+	return expression; 
+}
+
+string Main::callParenthesis() //edit this to call class InputProcessor 
+{
+	ParenthesisHandler parenthesis //creates object of parenthesis handler 
+	string expression=parenthesis.evalParenthesis() //keeps track of matching parenthesis, returns to main sub-expressions that need to be evaluated
+	//idea is to interact between parenthesis and main to evaulate each sub-expression one at a time and update the expression 
+	return expression; 
+}
