030c369aad1a6fc22f9f2d11b6731e4d0bb1a494
Lillian Brooks-Kanost <142836967+lbrookskanost@users.noreply.github.com>
2024-11-22 15:05:46 -0600

    added comments

--- addSubtract.cpp
+++ addSubtract.cpp
@@ -6,7 +6,7 @@ private:
     
 
 public:
-
+    
     template <typename T> T validateInput(T left, string operation, T right);
 
 };
@@ -14,13 +14,15 @@ public:
 
 template <typename T> T AddSub::validateInput(T left, string operation, T right){
     try {
+        //subtracts or adds based on the value of operation- operation should either be a "-" or "+"
         if (expression == "-") {
             return left - right;
         } else if (expression == "+")
             return left + right;
-    }
+    } //if something goes wrong, an error will be printed to cout, and the function will return -1 
     catch (...) {
         cout << "Error arose within AddSub, expression that was being validated was " << left << expression << right << endl;
+        return -1;
     }
 
 
