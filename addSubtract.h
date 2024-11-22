9d29b99c110a3ced0e79c89829b47b9c9d82662f
Lillian Brooks-Kanost <142836967+lbrookskanost@users.noreply.github.com>
2024-11-22 15:39:40 -0600

    Update addSubtract.h to match .cpp

--- addSubtract.h
+++ addSubtract.h
@@ -1,21 +1,18 @@
 //include gaurds 
 #ifndef ADDSUBTRACT_H //checks if ADDSUBTRACT_H has alrdy been defined, if true then this file will be skipped 
 #define ADDSUBTRACT_H //starts defenition for header file 
-
+#include <iostream>
 #include <string> //so we can return strings from functions 
 using namespace std;
 
 class addSub
 {
-    private:
-        string expression;
-public:
-
-    template <typename T> T  add(T left, T right);
-
-    template <typename T> T findOperators(string left, string oper, string right);
-
-    template <typename T> T  sub(T left,T right);
+    public:
+    
+    
+        template <typename T> T validateInput(T left, string operation, T right);
+    
+    
 
 };
 #endif //ends defenition of header file 
