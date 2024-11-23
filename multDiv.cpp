5feba657260f9ca6b489b0fb50adf9a97aa6d87d
JohneyMakeen <142916692+JohneyMakeen@users.noreply.github.com>
2024-11-22 18:25:34 -0600

    Update multDiv.cpp
    I did not test if it works, I worked on it via VS code

--- multDiv.cpp
+++ multDiv.cpp
@@ -1,32 +1,41 @@
-#include "multDiv.h" //header file for this file
-using namespace std;
+#include "multDiv.h" 
+#include <iostream> // Standard
+using namespace std; // cout, cin, etc.
 
 class MultDiv {
-private:
-    //I'm not sure what type of data we want to pass into these classes
-    //For now I left it as string. Feel free to change as needed
-    string expression;
 
 public:
+    template <typename T> 
+    T evaluateDiv(T left, T right) {
+    if (right == 0) {
+        cout << "Error: Division by zero is not allowed." << endl;
+        return -1; // This returns an error value 
+    }
+    return left/right; // perform normal division if otherwise
+  } 
 
-    template <typename T> T evaluateDiv(T left, T right);
-
-    template <typename T> T evaluateMult(T left, T right);
-
-
-    int validateInput(string left, string oper,string right);
+    template <typename T> 
+    T evaluateMult(T left, T right) {
+        return left * right; // perform normal multiplication
+    }
 
 
-
+    int validateInput(string left, string oper,string right){
+        if (oper != "*" && oper != "/") {
+            cout << "Error: Invalid operator, please use '*' or '/' " << endl;
+            return -1; // Returns error value 
+        } 
+        // I'm not sure if necessary, but just a thought, this is a error handeling to check if both left and right numbers are numeric, delete if not needed
+        for (char c : left + right) { // combine both strings 
+            if (!isdigit(c) && c != '.') { // Allow digits and a decimal point
+                cout << "Error: Invalid input. Make sure inputs are numbers" << endl;
+                return -1; // Invalid input 
+            } 
+        }
+        return 0; // Input is valid, yippee!!
+    }  
 };
 
-template <typename T> T MultDiv::evaluateDiv(T left, T right){
-    
-}
+ 
 
-template <typename T> T MultDiv::evaluateMult(T left, T right){
 
-}
-int MultDiv::validateInput(string left, string oper, string right){
-
-}
