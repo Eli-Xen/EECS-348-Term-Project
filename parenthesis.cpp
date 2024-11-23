69ad8b9f048e242c3e9a7b8f07f71a0e93f4ef6a
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-22 22:19:08 -0600

    updated skeleton for parenthesis.cpp
    added tokenizer, postfix, and expressionTree skeleton using vectors

--- parenthesis.cpp
+++ parenthesis.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
 #include <string>
 #include <sstream> 
+#include <vector>
 
 using namespace std;
 
 class Parenthesis {
 private:
 	//A varable to store the clean input after InputProcessor is called
-	string fullExpression;
-  string subExpression; 
-
+	string originalExpression; //original string expression after inputProcessor does its thing
+	vector<string> tokens; //vector that holds tokens after tokenizer runs 
+	vector<string> postfix; //vector that holds postfix toekns expression after postfix runs 
+	vector<string> final; //vector expressionTree runs 
 public:
-	//pointer to return array 
-	arr tokenizer(string expression); 
-	arr postfix(pointer to array after tokenizer); //call to expression Eval and incorporation back into expression eliminate parenthesis in fullExpression, could be incorporated into countParenthesis and just make evalParenthesis 
-	//make new array that stores result and reutrn pointer to that array 
-	arr expressionTree(pointer to array); 
+	//each has pointer to vector it returns 
+	vector<string> tokenizer(string expression); 
+	vector<string> postfix(const vector<string>& tokens); //call to expression Eval and incorporation back into expression eliminate parenthesis in fullExpression, could be incorporated into countParenthesis and just make evalParenthesis 
+	vector<string> expressionTree(const vector<string>& postfix); 
 };
 
 
-string Parenthesis::tokenizer(string expression) 
+
+vector<string> Parenthesis::tokenizer(string expression)
 {
-	
+	vector<string> tokens; //vector that stores result 
+
+
+
+
+	return tokens; //return vector directly 
 }
 
-string Parenthesis::postfix(pointer to tokenized array) //edit this to call class InputProcessor 
+vector<string> Parenthesis::postfix(const vector<string>& tokens) //passes vector by referance but uses const to avoid changes 
 {
-	expressionEval evaluator
+	vector<string> postfix; //vector that stores result 
+
+
+
+
+	return postfix; //return vector directly 
+}
+
+vector<string> Parenthesis::expressionTree(const vector<string>& postfix)
+{
+	vector<string> final; //vector that stores result; idk what exactly were returning yet 
+
+
+
+
+	return final; //return vector directly 
 }
