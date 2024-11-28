a4eb2ead8b16afafeccd3a4efe9aae03a7fa033e
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-11-27 20:48:15 -0600

    Update expo.h

--- expo.h
+++ expo.h
@@ -2,19 +2,15 @@
 #ifndef EXPO_H //checks if EXPO_H has alrdy been defined, if true then this file will be skipped 
 #define EXPO_H //starts defenition for header file 
 
-#include<bits/stdc++.h> 
 #include <string> //so we can return strings from functions 
 #include <iostream>
 #include <sstream> 
 using namespace std;
 
-class Expo {
-private:
-    string expression;
-
+class Expo 
+{
 public:
-
-    template <typename T> T evlExponent(T base, T power);
-
+    string evlExponent(string base, string power)//https://www.geeksforgeeks.org/cpp-string-to-float-double-and-vice-versa/# 
 };
+string Expo::evlExponent(string base, string power); 
 #endif //ends defenition of header file 
